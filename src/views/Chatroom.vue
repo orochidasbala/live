f1af0402b57ed7bd937bc952ae82f47bb778f294
Dothon <souvenirhallow@gmail.com>
2022-09-16 00:39:20 +0630

    redirect to login page when user logged out

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,9 +5,22 @@
 </template>
 
 <script>
+import { watch } from "@vue/runtime-core";
 import Navbar from "../components/Navbar";
+import router from "@/router";
+import getUser from "@/composable/getUser";
 export default {
     components: { Navbar },
+    setup() {
+        let { user } = getUser();
+        watch(user, () => {
+            if (!user.value) {
+                router.push({ name: "Welcome" });
+            } else {
+                console.log("you've something forgot, pls check your code");
+            }
+        });
+    },
 };
 </script>
 
